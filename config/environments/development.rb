befda7e3036d3f35370704854497f1e612cde015
hazim0001 <hazim_alsayed@hotmail.com>
2020-11-23 18:59:36 -0600

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -41,6 +41,8 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
