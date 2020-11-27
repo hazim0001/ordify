8b210395f2e32e02a82c37d45919562fe62aa830
Veronica Lopez <f.d.a.vero@hotmail.com>
2020-11-27 14:42:50 -0600

    fixed prices stripe schema migrations views controllers

--- db/migrate/20201124000209_create_menu_items.rb
+++ db/migrate/20201124000209_create_menu_items.rb
@@ -3,7 +3,6 @@ class CreateMenuItems < ActiveRecord::Migration[6.0]
     create_table :menu_items do |t|
       t.string :title
       t.text :description
-      t.float :item_price, default: 0.0
       t.references :restaurant, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
       t.boolean :active, default: true
