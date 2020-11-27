c76760643bfaf8b0128cdfc49381930b9091ec2b
Veronica Lopez <f.d.a.vero@hotmail.com>
2020-11-27 14:18:39 -0600

    fixed migration issues

--- db/migrate/20201124000105_create_orders.rb
+++ db/migrate/20201124000105_create_orders.rb
@@ -5,6 +5,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.references :table, null: false, foreign_key: true
       t.string :user_number
       t.boolean :sent, default: false
+      t.float :total_price, default: 0.0
       t.string :status, default: 'not paid'
       t.string :checkout_session_id
 
--- db/migrate/20201124001113_create_line_items.rb
+++ db/migrate/20201124001113_create_line_items.rb
@@ -4,6 +4,7 @@ class CreateLineItems < ActiveRecord::Migration[6.0]
       t.text :comment, default: ""
       t.integer :quantity, default: 1
       t.boolean :sent, default: false
+      t.float :total, default: 0.0
       t.references :order, null: false, foreign_key: true
       t.references :menu_item, null: false, foreign_key: true
 
