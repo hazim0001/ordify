2dae386029ffa55d2af1a1c931d62b7c129adf4a
hazim0001 <hazim_alsayed@hotmail.com>
2020-11-29 22:49:50 -0600

    clock with a date added to the kitchen view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -74,6 +74,25 @@ document.addEventListener('turbolinks:load', () => {
   incrementButtons.forEach(btn => {
     btn.addEventListener("click", increment);
   });
+
+  var tday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  var tmonth=["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+  function GetClock(){
+  var d=new Date();
+  var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getFullYear();
+  var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds();
+  if(nmin<=9) nmin="0"+nmin;
+  if(nsec<=9) nsec="0"+nsec;
+
+  var clocktext=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+"";
+  document.getElementById('clockbox').innerHTML=clocktext;
+  }
+
+  GetClock();
+  setInterval(GetClock,1000);
+
+
 });
 
 
