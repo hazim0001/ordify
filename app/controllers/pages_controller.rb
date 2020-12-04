293e25a0aa0c0b9d5d57e652aaabc16dc5ac705b
hazim0001 <hazim_alsayed@hotmail.com>
2020-12-03 18:30:21 -0600

    s

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,4 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_employee!, only: %i[home payment]
   def home
   end
-
-  def payment
-    # sleep(5)
-    # redirect_to new_table_order_url(@order.table)
-  end
 end
