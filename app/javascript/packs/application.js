a675ab6ae2311f03018ddbb1b08de37c64687be4
Veronica Lopez <f.d.a.vero@hotmail.com>
2020-12-04 13:58:11 -0600

    fixed loader and pizza modal trigger in menu items index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,36 +19,45 @@ require("channels")
 // Internal imports, e.g:
 import "bootstrap";
 import "jquery";
+import NProgress from "nprogress";
 
 // International Telephone Number with Flags and Country Dial Codes
 import intlTelInput from 'intl-tel-input';
 
 import { initKitchenOrderCable } from '../channels/kitchen_order_channel'
 
+
+
+// ----------------------------------------------------------------
+//Page Loader on Top
+
+document.addEventListener('turbolinks:request-start', () => {
+  NProgress.start();
+  console.log("working1")
+});
+
+
+document.addEventListener('turbolinks:request-end', () => {
+  NProgress.done();
+  console.log("working2")
+});
+
+// ----------------------------------------------------------------
+
 // import { initSelect2 } from '../components/init_select2';
-
 document.addEventListener('turbolinks:load', () => {
-
-  // ----------------------------------------------------------------
-
 
   // International Telephone Number with Flags and Country Dial Codes
   const phone = document.getElementById("phone");
 
   if (phone) {
     intlTelInput(phone, {
-    // allowExtensions: true,
-    // autoFormat: false,
-    // autoHideDialCode: false,
-    // autoPlaceholder: false,
-    // defaultCountry: "auto",
+    initialCountry: 'mx',
     // ipinfoToken: "yolo",
-    // nationalMode: false,
     // numberType: "MOBILE",
-    // //onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
-    // //preferredCountries: ['cn', 'jp'],
-    // preventInvalidNumbers: true,
-    utilsScript: "utils.js"
+    formatOnDisplay: true,
+    preferredCountries: ['mx', 'us', 'gb'],
+    utilsScript: "/utils.js"
     });
   }
 
@@ -135,23 +144,28 @@ document.addEventListener('turbolinks:load', () => {
 
   var btnTrigger = document.getElementById("modal-0line-items-trigger")
   var modalNoItems = document.getElementById("noItemsNotification")
+  var closeBtn = document.getElementsByClassName("close")[0];
 
   if (btnTrigger) {
     btnTrigger.addEventListener('click', (e)=> {
       modalNoItems.classList.add("showSpecial");
     });
-  }
-
-  var closeBtn = document.getElementsByClassName("close")[0];
-
-  if (closeBtn) {
     closeBtn.addEventListener('click', (e)=> {
       modalNoItems.classList.remove("showSpecial");
     });
   }
 
+  var btnTrigger2 = document.getElementById("modal-0line-items-trigger2")
+  var modalNoItems2 = document.getElementById("noItemsNotification2")
 
-
+  if (btnTrigger2) {
+    btnTrigger2.addEventListener('click', (e)=> {
+      modalNoItems2.classList.add("showSpecial");
+    });
+    closeBtn.addEventListener('click', (e)=> {
+      modalNoItems2.classList.remove("showSpecial");
+    });
+  }
 
   // PIZZA Animation
 
@@ -160,135 +174,138 @@ document.addEventListener('turbolinks:load', () => {
   let toRadians = (deg) => deg * Math.PI / 180
   let map = (val, a1, a2, b1, b2) => b1 + (val - a1) * (b2 - b1) / (a2 - a1)
 
-  class Pizza {
-    constructor(id) {
-      this.canvas = document.getElementById(id)
-      this.ctx = this.canvas.getContext('2d')
+  if (modalNoItems||modalNoItems2) {
 
-      this.sliceCount = 6
-      this.sliceSize = 80
+    class Pizza {
+      constructor(id) {
+        this.canvas = document.getElementById(id)
+        this.ctx = this.canvas.getContext('2d')
 
-      this.width = this.height = this.canvas.height = this.canvas.width = this.sliceSize * 2 + 50
-      this.center = this.height / 2 | 0
+        this.sliceCount = 6
+        this.sliceSize = 80
 
-      this.sliceDegree = 360 / this.sliceCount
-      this.sliceRadians = toRadians(this.sliceDegree)
-      this.progress = 0
-      this.cooldown = 10
+        this.width = this.height = this.canvas.height = this.canvas.width = this.sliceSize * 2 + 50
+        this.center = this.height / 2 | 0
 
-    }
-
-    update() {
-      let ctx = this.ctx
-      ctx.clearRect(0, 0, this.width, this.height)
-
-      if (--this.cooldown < 0) this.progress += this.sliceRadians*0.01 + this.progress * 0.07
-
-      ctx.save()
-      ctx.translate(this.center, this.center)
-
-      for (let i = this.sliceCount - 1; i > 0; i--) {
-
-        let rad
-        if (i === this.sliceCount - 1) {
-          let ii = this.sliceCount - 1
-
-          rad = this.sliceRadians * i + this.progress
-
-          ctx.strokeStyle = '#FBC02D'
-          cheese(ctx, rad, .9, ii, this.sliceSize, this.sliceDegree)
-          cheese(ctx, rad, .6, ii, this.sliceSize, this.sliceDegree)
-          cheese(ctx, rad, .5, ii, this.sliceSize, this.sliceDegree)
-          cheese(ctx, rad, .3, ii, this.sliceSize, this.sliceDegree)
-
-        } else rad = this.sliceRadians * i
-
-        // border
-        ctx.beginPath()
-        ctx.lineCap = 'butt'
-        ctx.lineWidth = 11
-        ctx.arc(0, 0, this.sliceSize, rad, rad + this.sliceRadians)
-        ctx.strokeStyle = '#F57F17'
-        ctx.stroke()
-
-        // slice
-        let startX = this.sliceSize * Math.cos(rad)
-        let startY = this.sliceSize * Math.sin(rad)
-        let endX = this.sliceSize * Math.cos(rad + this.sliceRadians)
-        let endY = this.sliceSize * Math.sin(rad + this.sliceRadians)
-        let varriation = [0.9,0.7,1.1,1.2]
-        ctx.fillStyle = '#FBC02D'
-        ctx.beginPath()
-        ctx.moveTo(0, 0)
-        ctx.lineTo(startX, startY)
-        ctx.arc(0, 0, this.sliceSize, rad, rad + this.sliceRadians)
-        ctx.lineTo(0, 0)
-        ctx.closePath()
-        ctx.fill()
-        ctx.lineWidth = .3
-        ctx.stroke()
-
-        // meat
-        let x = this.sliceSize * .65 * Math.cos(rad + this.sliceRadians / 2)
-        let y = this.sliceSize * .65 * Math.sin(rad + this.sliceRadians / 2)
-        ctx.beginPath()
-        ctx.arc(x, y, this.sliceDegree / 6, 0, 2 * Math.PI)
-        ctx.fillStyle = '#D84315'
-        ctx.fill()
-
-      }
-
-      ctx.restore()
-
-      if (this.progress > this.sliceRadians) {
-        ctx.translate(this.center, this.center)
-        ctx.rotate(-this.sliceDegree * Math.PI / 180)
-        ctx.translate(-this.center, -this.center)
-
+        this.sliceDegree = 360 / this.sliceCount
+        this.sliceRadians = toRadians(this.sliceDegree)
         this.progress = 0
-        this.cooldown = 20
+        this.cooldown = 10
+
+      }
+
+      update() {
+        let ctx = this.ctx
+        ctx.clearRect(0, 0, this.width, this.height)
+
+        if (--this.cooldown < 0) this.progress += this.sliceRadians*0.01 + this.progress * 0.07
+
+        ctx.save()
+        ctx.translate(this.center, this.center)
+
+        for (let i = this.sliceCount - 1; i > 0; i--) {
+
+          let rad
+          if (i === this.sliceCount - 1) {
+            let ii = this.sliceCount - 1
+
+            rad = this.sliceRadians * i + this.progress
+
+            ctx.strokeStyle = '#FBC02D'
+            cheese(ctx, rad, .9, ii, this.sliceSize, this.sliceDegree)
+            cheese(ctx, rad, .6, ii, this.sliceSize, this.sliceDegree)
+            cheese(ctx, rad, .5, ii, this.sliceSize, this.sliceDegree)
+            cheese(ctx, rad, .3, ii, this.sliceSize, this.sliceDegree)
+
+          } else rad = this.sliceRadians * i
+
+          // border
+          ctx.beginPath()
+          ctx.lineCap = 'butt'
+          ctx.lineWidth = 11
+          ctx.arc(0, 0, this.sliceSize, rad, rad + this.sliceRadians)
+          ctx.strokeStyle = '#F57F17'
+          ctx.stroke()
+
+          // slice
+          let startX = this.sliceSize * Math.cos(rad)
+          let startY = this.sliceSize * Math.sin(rad)
+          let endX = this.sliceSize * Math.cos(rad + this.sliceRadians)
+          let endY = this.sliceSize * Math.sin(rad + this.sliceRadians)
+          let varriation = [0.9,0.7,1.1,1.2]
+          ctx.fillStyle = '#FBC02D'
+          ctx.beginPath()
+          ctx.moveTo(0, 0)
+          ctx.lineTo(startX, startY)
+          ctx.arc(0, 0, this.sliceSize, rad, rad + this.sliceRadians)
+          ctx.lineTo(0, 0)
+          ctx.closePath()
+          ctx.fill()
+          ctx.lineWidth = .3
+          ctx.stroke()
+
+          // meat
+          let x = this.sliceSize * .65 * Math.cos(rad + this.sliceRadians / 2)
+          let y = this.sliceSize * .65 * Math.sin(rad + this.sliceRadians / 2)
+          ctx.beginPath()
+          ctx.arc(x, y, this.sliceDegree / 6, 0, 2 * Math.PI)
+          ctx.fillStyle = '#D84315'
+          ctx.fill()
+
+        }
+
+        ctx.restore()
+
+        if (this.progress > this.sliceRadians) {
+          ctx.translate(this.center, this.center)
+          ctx.rotate(-this.sliceDegree * Math.PI / 180)
+          ctx.translate(-this.center, -this.center)
+
+          this.progress = 0
+          this.cooldown = 20
+        }
+
       }
 
     }
 
+    function cheese(ctx, rad, multi, ii, sliceSize, sliceDegree) {
+      let x1 = sliceSize * multi * Math.cos(toRadians(ii * sliceDegree) - .2)
+      let y1 = sliceSize * multi * Math.sin(toRadians(ii * sliceDegree) - .2)
+      let x2 = sliceSize * multi * Math.cos(rad + .2)
+      let y2 = sliceSize * multi * Math.sin(rad + .2)
+
+      let csx = sliceSize * Math.cos(rad)
+      let csy = sliceSize * Math.sin(rad)
+
+      var d = Math.sqrt((x1 - csx) * (x1 - csx) + (y1 - csy) * (y1 - csy))
+      ctx.beginPath()
+      ctx.lineCap = 'round'
+
+      let percentage = map(d, 15, 70, 1.2, 0.2)
+
+      let tx = x1 + (x2 - x1) * percentage
+      let ty = y1 + (y2 - y1) * percentage
+      ctx.moveTo(x1, y1)
+      ctx.lineTo(tx, ty)
+
+      tx = x2 + (x1 - x2) * percentage
+      ty = y2 + (y1 - y2) * percentage
+      ctx.moveTo(x2, y2)
+      ctx.lineTo(tx, ty)
+
+      ctx.lineWidth = map(d, 0, 100, 20, 2)
+      ctx.stroke()
+    }
+
+    let pizza = new Pizza('pizza')
+
+    ;(function update() {
+      requestAnimationFrame(update)
+      pizza.update()
+
+    }())
   }
-
-  function cheese(ctx, rad, multi, ii, sliceSize, sliceDegree) {
-    let x1 = sliceSize * multi * Math.cos(toRadians(ii * sliceDegree) - .2)
-    let y1 = sliceSize * multi * Math.sin(toRadians(ii * sliceDegree) - .2)
-    let x2 = sliceSize * multi * Math.cos(rad + .2)
-    let y2 = sliceSize * multi * Math.sin(rad + .2)
-
-    let csx = sliceSize * Math.cos(rad)
-    let csy = sliceSize * Math.sin(rad)
-
-    var d = Math.sqrt((x1 - csx) * (x1 - csx) + (y1 - csy) * (y1 - csy))
-    ctx.beginPath()
-    ctx.lineCap = 'round'
-
-    let percentage = map(d, 15, 70, 1.2, 0.2)
-
-    let tx = x1 + (x2 - x1) * percentage
-    let ty = y1 + (y2 - y1) * percentage
-    ctx.moveTo(x1, y1)
-    ctx.lineTo(tx, ty)
-
-    tx = x2 + (x1 - x2) * percentage
-    ty = y2 + (y1 - y2) * percentage
-    ctx.moveTo(x2, y2)
-    ctx.lineTo(tx, ty)
-
-    ctx.lineWidth = map(d, 0, 100, 20, 2)
-    ctx.stroke()
-  }
-
-  let pizza = new Pizza('pizza')
-
-  ;(function update() {
-    requestAnimationFrame(update)
-    pizza.update()
-
-  }())
 
   // ----------------------------------------------------------------
 
@@ -323,6 +340,11 @@ document.addEventListener('turbolinks:load', () => {
   setInterval(GetClock,1000);
 
 });
+
+  // ----------------------------------------------------------------
+
+
+  // Page loader
 
 
 
