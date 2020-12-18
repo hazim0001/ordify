ce10cdfcd9bdf3d7fa71e538491a3796145e4bea
hazim0001 <hazim_alsayed@hotmail.com>
2020-12-18 07:07:23 +0200

    added column in orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_084638) do
+ActiveRecord::Schema.define(version: 2020_12_18_050454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -178,6 +178,7 @@ ActiveRecord::Schema.define(version: 2020_12_17_084638) do
     t.boolean "order_deleted", default: false
     t.string "order_deleted_by"
     t.datetime "order_deleted_at"
+    t.text "deletion_reason"
     t.index ["table_id"], name: "index_orders_on_table_id"
   end
 
