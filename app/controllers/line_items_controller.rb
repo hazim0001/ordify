40cafbc0439c5d292c5a7ce1ca1fd80cad88c29c
hazim0001 <hazim_alsayed@hotmail.com>
2020-12-23 10:44:56 +0200

    re-designed manager menu view

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -40,7 +40,6 @@ class LineItemsController < ApplicationController
       update_totals_in_line_item_and_order
       update_inventory
       @line_item.destroy if @line_item.quantity.zero?
-      # raise
       redirect_to order_line_items_path
     end
   end
