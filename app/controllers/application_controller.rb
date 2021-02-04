2bfd408d3d81127a86b5dc8a58f40acdd1e37054
hazim0001 <hazim_alsayed@hotmail.com>
2021-02-04 16:44:10 +0200

    added comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,9 @@ class ApplicationController < ActionController::Base
 
   # after_action :verify_authorized, except: :index, unless: :skip_pundit?
   # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
-  # def after_sign_in_path_for(resource)
-  #   if resource.role == "cook"
-  #     restaurant_tables_path(resource.restaurant)
-  #   elsif resource.role == "manager" && resource.admin? == true
-  #     # restaurant_inventories_path(resource.restaurant)
-  #     rails_admin_path
-  #   end
-  # end
 
+
+ # devise routing after sign in
   def after_sign_in_path_for(resource)
     if resource.role == "cook"
       restaurant_tables_path(resource.restaurant)
@@ -39,6 +33,8 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
+
+ # the defualt pundit user instead of current user
   def pundit_user
     current_employee
   end
