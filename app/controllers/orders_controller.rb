ed31ed5efb818511c14db5b53e69b3d19e95b63b
hazim0001 <hazim_alsayed@hotmail.com>
2021-02-20 06:37:36 +0200

    new config cleaning controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -21,33 +21,33 @@ class OrdersController < ApplicationController
   end
 
   def create
-    if manager_is_here?
-      # raise
-      @order = Order.create(table_id: order_params[:table].to_i, user_number: order_params[:user_number])
-      params[:menu_item_id].each_with_index do |id, index|
-        line_item = LineItem.create(
-          menu_item_id: id.to_i, order: @order, quantity: params[:quantity][index], ordered: true, received_at: Time.now
-        )
-        # updating line item total and order total
-        line_item.update(total_cents: line_item.menu_item.item_price_cents * line_item.quantity)
-        # updating inventory
-        line_item.menu_item.ingredients.each do |ingredient|
-          portion = ingredient.ingredient_portion_size_grams
-          quantity = line_item.quantity
-          current_stock = ingredient.ingredient_inventory.stock_amount_grams
-          ingredient.ingredient_inventory.update(stock_amount_grams: current_stock - (portion * quantity))
-        end
-      end
-      # where("dispatched_from_kitchen= ?", false)
-      undispatched_line_items = @order.table.line_items.not_dispatched_from_kitchen
-      KitchenOrderChannel.broadcast_to(
-        @order.table, render_to_string(partial: "new_line_item", locals: { lines: undispatched_line_items })
-      )
-      sub_total = LineItem.where(order: @order).sum(:total_cents)
-      @order.update(total_price_cents: sub_total, sent: true)
-      flash[:notice] = "Order##{@order.id} has been created and sent to the kitchen"
-      redirect_to new_restaurant_order_path(current_employee_restaurant)
-    else
+    # if manager_is_here?
+    #   # raise
+    #   @order = Order.create(table_id: order_params[:table].to_i, user_number: order_params[:user_number])
+    #   params[:menu_item_id].each_with_index do |id, index|
+    #     line_item = LineItem.create(
+    #       menu_item_id: id.to_i, order: @order, quantity: params[:quantity][index], ordered: true, received_at: Time.now
+    #     )
+    #     # updating line item total and order total
+    #     line_item.update(total_cents: line_item.menu_item.item_price_cents * line_item.quantity)
+    #     # updating inventory
+    #     # line_item.menu_item.ingredients.each do |ingredient|
+    #     #   portion = ingredient.ingredient_portion_size_grams
+    #     #   quantity = line_item.quantity
+    #     #   current_stock = ingredient.ingredient_inventory.stock_amount_grams
+    #     #   ingredient.ingredient_inventory.update(stock_amount_grams: current_stock - (portion * quantity))
+    #     # end
+    #   end
+    #   # where("dispatched_from_kitchen= ?", false)
+    #   undispatched_line_items = @order.table.line_items.not_dispatched_from_kitchen
+    #   KitchenOrderChannel.broadcast_to(
+    #     @order.table, render_to_string(partial: "new_line_item", locals: { lines: undispatched_line_items })
+    #   )
+    #   sub_total = LineItem.where(order: @order).sum(:total_cents)
+    #   @order.update(total_price_cents: sub_total, sent: true)
+    #   flash[:notice] = "Order##{@order.id} has been created and sent to the kitchen"
+    #   redirect_to new_restaurant_order_path(current_employee_restaurant)
+    # else
       @table = Table.find(params[:table_id])
       @order = Order.find_or_initialize_by(user_number: order_params[:user_number], status: "not paid") # Order.new(order_params)
       @order.table = @table
@@ -57,7 +57,7 @@ class OrdersController < ApplicationController
       else
         render :new
       end
-    end
+    # end
   end
 
   def index
@@ -76,21 +76,21 @@ class OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    if manager_is_here?
-      @order.update(table_id: order_params[:table].to_i, status: order_params[:status])
-      @order.update(from_order_to_checkout: Time.now - @order.created_at) if @order.status == "paid"
-      redirect_to orders_path
-    else
-      @order.line_items.each { |line| line.update(ordered: true, received_at: Time.now) }
-      @order.update(sent: true, dispatched: false)
-      undispatched_line_items = @order.table.line_items.not_dispatched_from_kitchen # where("dispatched_from_kitchen= ?", false)
-      KitchenOrderChannel.broadcast_to(
-        @order.table, render_to_string(partial: "new_line_item", locals: { lines: undispatched_line_items })
-      )
-      sleep(4)
-      stripe_order
-      redirect_back fallback_location: proc { order_line_items_path(@order) }
-    end
+    # if manager_is_here?
+    #   @order.update(table_id: order_params[:table].to_i, status: order_params[:status])
+    #   @order.update(from_order_to_checkout: Time.now - @order.created_at) if @order.status == "paid"
+    #   redirect_to orders_path
+    # else
+    @order.line_items.each { |line| line.update(ordered: true, received_at: Time.now) }
+    @order.update(sent: true, dispatched: false)
+    undispatched_line_items = @order.table.line_items.not_dispatched_from_kitchen # where("dispatched_from_kitchen= ?", false)
+    KitchenOrderChannel.broadcast_to(
+      @order.table, render_to_string(partial: "new_line_item", locals: { lines: undispatched_line_items })
+    )
+    sleep(2)
+    # stripe_order
+    redirect_back fallback_location: proc { order_line_items_path(@order) }
+    # end
   end
 
   def destroy
@@ -108,20 +108,20 @@ class OrdersController < ApplicationController
       order_deleted_by: current_employee.name,
       deletion_reason: order_params[:deletion_reason]
     )
-    return_inventory
+    # return_inventory
     redirect_back fallback_location: proc { orders_path }
   end
 
-  def dispatch_notify
-    @order = Order.find(params[:id])
-    update_line_items
-    @order.table.line_items.each { |line| line.update(dispatched_from_kitchen: true) }
-    @order.table.orders.each do |order|
-      order.update(dispatched: true)
-      twilio_sms
-    end
-    redirect_back fallback_location: proc { restaurant_tables_path(@order.restaurant) }
-  end
+  # def dispatch_notify
+  #   @order = Order.find(params[:id])
+  #   update_line_items
+  #   @order.table.line_items.each { |line| line.update(dispatched_from_kitchen: true) }
+  #   @order.table.orders.each do |order|
+  #     order.update(dispatched: true)
+  #     twilio_sms
+  #   end
+  #   redirect_back fallback_location: proc { restaurant_tables_path(@order.restaurant) }
+  # end
 
   def pay
     auth_token = authentication_request
@@ -139,54 +139,54 @@ class OrdersController < ApplicationController
     end
   end
 
-  def return_inventory
-    @order.line_items.each do |line_item|
-      line_item.menu_item.ingredients.each do |ingredient|
-        portion = ingredient.ingredient_portion_size_grams
-        quantity = line_item.quantity
-        current_stock = ingredient.ingredient_inventory.stock_amount_grams
-        ingredient.ingredient_inventory.update(stock_amount_grams: current_stock + (portion * quantity))
-      end
+  # def return_inventory
+  #   @order.line_items.each do |line_item|
+  #     line_item.menu_item.ingredients.each do |ingredient|
+  #       portion = ingredient.ingredient_portion_size_grams
+  #       quantity = line_item.quantity
+  #       current_stock = ingredient.ingredient_inventory.stock_amount_grams
+  #       ingredient.ingredient_inventory.update(stock_amount_grams: current_stock + (portion * quantity))
+  #     end
 
-      next if line_item.extras.any?
+  #     next if line_item.extras.any?
 
-      line_item.extras.each do |extra|
-        portion = extra.size_grams
-        current_stock = extra.ingredient_inventory.stock_amount_grams
-        extra.ingredient_inventory.update(stock_amount_grams: current_stock - portion)
-      end
-    end
-  end
+  #     line_item.extras.each do |extra|
+  #       portion = extra.size_grams
+  #       current_stock = extra.ingredient_inventory.stock_amount_grams
+  #       extra.ingredient_inventory.update(stock_amount_grams: current_stock - portion)
+  #     end
+  #   end
+  # end
 
-  def stripe_order
-    # session = Stripe::Checkout::Session.create(
-    #   payment_method_types: ['card'],
-    #   line_items: [{
-    #     name: @order.user_number,
-    #     amount: @order.total_price_cents,
-    #     currency: 'usd',
-    #     quantity: 1
-    #   }],
-    #   mode: 'payment',
-    #   success_url: payment_url, # to create a thank u page for ur payment
-    #   cancel_url: categories_url # render a notice tell him to try a dif card
-    # )
-    # @order.update(checkout_session_id: session.id)
-  end
+  # def stripe_order
+  #   # session = Stripe::Checkout::Session.create(
+  #   #   payment_method_types: ['card'],
+  #   #   line_items: [{
+  #   #     name: @order.user_number,
+  #   #     amount: @order.total_price_cents,
+  #   #     currency: 'usd',
+  #   #     quantity: 1
+  #   #   }],
+  #   #   mode: 'payment',
+  #   #   success_url: payment_url, # to create a thank u page for ur payment
+  #   #   cancel_url: categories_url # render a notice tell him to try a dif card
+  #   # )
+  #   # @order.update(checkout_session_id: session.id)
+  # end
 
-  def twilio_sms
-    # account_sid = ENV['ACCOUNT_SID']
-    # auth_token = ENV['AUTH_TOKEN']
-    # client = Twilio::REST::Client.new(account_sid, auth_token)
-    # from = '+12056547036' # Your Twilio number
-    # to = '+529841452138'          # order.user_number # Your mobile phone number
-    # client.messages.create(
-    #   from: from,
-    #   to: to,
-    #   body: "Hola from Ordify!, Your Meal is on the way"
-    # )
-    # return
-  end
+  # def twilio_sms
+  #   # account_sid = ENV['ACCOUNT_SID']
+  #   # auth_token = ENV['AUTH_TOKEN']
+  #   # client = Twilio::REST::Client.new(account_sid, auth_token)
+  #   # from = '+12056547036' # Your Twilio number
+  #   # to = '+529841452138'          # order.user_number # Your mobile phone number
+  #   # client.messages.create(
+  #   #   from: from,
+  #   #   to: to,
+  #   #   body: "Hola from Ordify!, Your Meal is on the way"
+  #   # )
+  #   # return
+  # end
 
   # pay mob methods
 
--- app/jobs/inventory_check_job.rb
+++ app/jobs/inventory_check_job.rb
@@ -1,13 +1,13 @@
-class InventoryCheckJob < ApplicationJob
-  queue_as :default
+# class InventoryCheckJob < ApplicationJob
+#   queue_as :default
 
-  def perform
-    # to group inventory by restaurant and send one email to its manager
-    triggered_inventory = Inventory.where("trigger_limit > stock_amount_grams")
-    triggered_inventory.each do |inventory|
-      restaurant_manager = inventory.restaurant.employees.find_by(role: "manager").email
-      message = "#{inventory.menu_item.title} is running low"
-      InventoryMailer.submission(message, restaurant_manager).deliver
-    end
-  end
-end
+#   def perform
+#     # to group inventory by restaurant and send one email to its manager
+#     triggered_inventory = Inventory.where("trigger_limit > stock_amount_grams")
+#     triggered_inventory.each do |inventory|
+#       restaurant_manager = inventory.restaurant.employees.find_by(role: "manager").email
+#       message = "#{inventory.menu_item.title} is running low"
+#       InventoryMailer.submission(message, restaurant_manager).deliver
+#     end
+#   end
+# end
