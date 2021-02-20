156e496526c56314780f1bd86a20b8b06841138e
hazim0001 <hazim_alsayed@hotmail.com>
2021-02-20 06:37:50 +0200

    new config cleaning controllers

--- app/mailers/inventory_mailer.rb
+++ app/mailers/inventory_mailer.rb
@@ -1,8 +1,8 @@
-class InventoryMailer < ApplicationMailer
-  default from: 'johnoudai@gmail.com'
+# class InventoryMailer < ApplicationMailer
+#   default from: 'johnoudai@gmail.com'
 
-  def submission(message, email)
-    @message = message
-    mail(to: email, subject: 'test')
-  end
-end
+#   def submission(message, email)
+#     @message = message
+#     mail(to: email, subject: 'test')
+#   end
+# end
